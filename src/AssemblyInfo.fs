e3bdf1aad1710ab9eec14b33858ab1c2f7bd95e3
Ian Moore <ian.mark.moore@gmail.com>
2017-05-31 20:00:52 -0500

    commit missing file

---
+++ src/AssemblyInfo.fs
@@ -0,0 +1,10 @@
+namespace System
+  
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FsDynamics.Crm")>]
+[<assembly: AssemblyProductAttribute("FsDynamics.Crm")>]
+[<assembly: AssemblyDescriptionAttribute("F# friendly functions for Dynamics 365.")>]
+[<assembly: AssemblyVersionAttribute("0.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.0")>]
+do ()
